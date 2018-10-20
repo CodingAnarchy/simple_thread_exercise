87638f88e43c35da3d01616b36848efbb0e90c0e
Matt Tanous <mtanous22@gmail.com>
2018-10-20 18:45:36 -0400

    Test for duration and sum on the second project set

--- spec/project_set_spec.rb
+++ spec/project_set_spec.rb
@@ -13,4 +13,20 @@ describe ProjectSet do
       expect(first_set.reimbursement).to eq(165)
     end
   end
+
+  context "set #2" do
+    let(:first_project) { Project.new("2015-9-1", "2015-9-1", "low") }
+    let(:second_project) { Project.new("2015-9-2", "2015-9-6", "high") }
+    let(:third_project) { Project.new("2015-9-6", "2015-9-8", "low") }
+    let(:second_set) { ProjectSet.new([first_project, second_project, third_project]) }
+
+    it "has the duration between 9/1 and 9/8" do
+      expect(second_set.duration).to eq((Date.parse("2015-9-1")..Date.parse("2015-9-8")))
+    end
+
+    it "has the correct sum for the second project set" do
+      # Sum is (2 * 45) for low-cost travel days on both ends of set + (5 * 85) for the high cost project and 75 for the low cost project non-overlapping day = 590
+      expect(second_set.reimbursement).to eq(590)
+    end
+  end
 end
