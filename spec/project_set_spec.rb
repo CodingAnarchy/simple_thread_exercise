e7095e83a4ae580fda0446f036a123091a93997a
Matt Tanous <mtanous22@gmail.com>
2018-10-20 18:48:59 -0400

    Test for duration and sum on third project set - no changes need to main code

--- spec/project_set_spec.rb
+++ spec/project_set_spec.rb
@@ -29,4 +29,20 @@ describe ProjectSet do
       expect(second_set.reimbursement).to eq(590)
     end
   end
+
+  context "set #3" do
+    let(:first_project) { Project.new("2015-9-1", "2015-9-3", "low") }
+    let(:second_project) { Project.new("2015-9-5", "2015-9-7", "high") }
+    let(:third_project) { Project.new("2015-9-8", "2015-9-8", "high") }
+    let(:third_set) { ProjectSet.new([first_project, second_project, third_project]) }
+
+    it "has the duration between 9/1 and 9/8" do
+      expect(third_set.duration).to eq((Date.parse("2015-9-1")..Date.parse("2015-9-8")))
+    end
+
+    it "has the correct sum for the third project set" do
+      # Sum is 45 (low-cost travel) + 75 (low cost full) + 45 (low cost travel) + 0 (none) + 55 (high cost travel) + 2 * 85 (high cost full) + 55 (high cost travel) = 445
+      expect(third_set.reimbursement).to eq(445)
+    end
+  end
 end
